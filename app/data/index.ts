7e9f6ee79329e123fb6c13b2bd507a3d2617d65f
IT21214134 <it21214134@my.sliit.lk>
2025-05-18 16:06:46 +0530

    Added timeline of the website

---
+++ app/data/index.ts
@@ -0,0 +1,7 @@
+export const navItems = [
+    { name: "Project Scope", link: "#scope" },
+    { name: "Milestones", link: "#milestones" },
+    { name: "Downloads", link: "#downloads" },
+    { name: "About Us", link: "#about" },
+    { name: "Contact Us", link: "#contact" },
+];
