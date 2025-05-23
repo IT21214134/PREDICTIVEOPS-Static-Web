7d77fa0770ca50e1d88213cfd04c9a80a45acd0e
IsuriYapa <95137248+IT21214134@users.noreply.github.com>
2025-05-23 22:34:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -15,13 +15,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//         domains: ['assets.aceternity.com', 'mysliit-my.sharepoint.com'],
-//       },
-//       output: 'export',
-// };
-
-// export default nextConfig;
